bc204b641a6e7f478db196b5e67fd4b696bea7d3
Aston Monteiro <astonmonteiro08@gmail.com>
2026-03-12 18:33:31 +0530

    user login backend

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+name:{
+type:String,
+required:true
+},
+
+email:{
+type:String,
+required:true,
+unique:true
+},
+
+password:{
+type:String
+},
+
+googleId:{
+type:String
+},
+
+role:{
+type:String,
+default:"student"
+}
+
+},{timestamps:true})
+
+export default mongoose.model("User",userSchema)
