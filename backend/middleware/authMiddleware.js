1c2e60c8452f8dac4f63936e3f3802f6f3bf2eb6
Aston Monteiro <astonmonteiro08@gmail.com>
2026-03-12 20:33:55 +0530

    updated structure

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,24 +1,24 @@
 import jwt from "jsonwebtoken"
 
-const authMiddleware = (req,res,next)=>{
+const authMiddleware = (req, res, next) => {
 
-const token = req.headers.authorization
+  const token = req.headers.authorization
 
-if(!token){
-return res.status(401).json({message:"No token"})
-}
+  if (!token) {
+    return res.status(401).json({ message: "No token" })
+  }
 
-try{
+  try {
 
-const decoded = jwt.verify(token,process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-req.user = decoded
+    req.user = decoded
 
-next()
+    next()
 
-}catch(err){
-res.status(401).json({message:"Invalid token"})
-}
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" })
+  }
 
 }
 
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,31 +1,33 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
 
-name:{
-type:String,
-required:true
-},
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
 
-email:{
-type:String,
-required:true,
-unique:true
-},
+    password: {
+      type: String
+    },
 
-password:{
-type:String
-},
+    googleId: {
+      type: String
+    },
 
-googleId:{
-type:String
-},
+    role: {
+      type: String,
+      default: "student"
+    }
 
-role:{
-type:String,
-default:"student"
-}
+  },
+  { timestamps: true }
+)
 
-},{timestamps:true})
-
-export default mongoose.model("User",userSchema)
+export default mongoose.model("User", userSchema)
