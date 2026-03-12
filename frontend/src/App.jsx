b8c3f176c509da7872bbd7bebc45c2fead2ba654
Aston Monteiro <astonmonteiro08@gmail.com>
2026-03-12 16:04:55 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CreateTodo from './component/CreateTodo'
+import TodoList from './component/TodoList'
+import UpdateTodo from './component/UpdateTodo'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TodoList />} />
+        <Route path='/create' element={<CreateTodo />} />
+        <Route path='/edit/:id' element={<UpdateTodo />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/component/UpdateTodo.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+
+function UpdateTodo(){
+
+ 
+
+  return(
+
+    <div>
+      <h1>Update Todo</h1>
+    </div>
+  )
+}
+
+export default UpdateTodo
